49cb438206a25acdefc459455670590fb8769838
joe fisher <jhanajon@gmail.com>
2018-07-09 05:57:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function yourAnimal() {
 
 function add2(n) {
   const 'two' = 2
-  return 'two' + n 
+  return 'two' + n
 
   // Feel free to move things around!
-  
+
 }
 
 var funkyFunction = function() {
